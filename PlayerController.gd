b72bdb1030af5a8afaa2ada070cc353949979384
Sulfurous-Impersonation <june@watson-net.com>
2023-01-01 18:29:02 -0800

    moved dash back to _input, detached camera, fixed slashNoise

--- PlayerController.gd
+++ PlayerController.gd
@@ -15,43 +15,28 @@ var motion = Vector2()
 var facingRight = true
 var falling = false
 var hasLanded = true
-var dashing = false
 
 # variables for audio handling
 var played = false
 
 # onready references to child nodes
 onready var sprite := $Sprite
-onready var slashSprite := $Slash
 onready var anim := $AnimationPlayer
 onready var dashDelayTimer := $DashDelayTimer
 
 export var dashDelay: float = 0.5
 
-func _physics_process(delta):
-	# falling physics
-	motion.y += GRAVITY
-	if motion.y > MAX_FALL_SPEED:
-		motion.y = MAX_FALL_SPEED
-	
-	# determine vars used for dashing
+func _input(event):
+	# determine hasLanded
 	if is_on_floor():
 		hasLanded = true
-	dashing = false
-	
-	# used for animation facing
-	if facingRight == true:
-		sprite.scale.x = 1
-	else:
-		sprite.scale.x = -1
-	
-	# handle dashing
-	if Input.is_action_just_pressed("Dash") and dashDelayTimer.is_stopped() and hasLanded: # on click
+
+	# handle dash + associated animation
+	if event is InputEventMouseButton and dashDelayTimer.is_stopped() and hasLanded: # on click
 		# hasn't landed since dashing yet
 		print("Dashing")
 		hasLanded = false
-		dashing = true
-		
+
 		# start timer
 		dashDelayTimer.start(dashDelay)
 
@@ -60,9 +45,11 @@ func _physics_process(delta):
 
 		# get local mouse direction, calculate motion
 		var mouse_direction = get_local_mouse_position().normalized()
-		motion = Vector2(mouse_direction.x, mouse_direction.y) * MAX_SPEED * 10
+		motion = Vector2(mouse_direction.x, mouse_direction.y) * MAX_SPEED/2
 		print("Mouse position:\t" + String(get_viewport().get_mouse_position()))
 		print("Global Transform:\t\t\t" + String(sprite.position))
+
+		var t = get_physics_process_delta_time()
 
 		# used for animation facing
 		if mouse_direction.x > sprite.position.x: # dashing right
@@ -74,7 +61,22 @@ func _physics_process(delta):
 
 		# animate
 		anim.play("Dash")
-	
+
+		# apply movement
+		move_and_collide(motion)
+
+func _physics_process(delta):
+	# falling physics
+	motion.y += GRAVITY
+	if motion.y > MAX_FALL_SPEED:
+		motion.y = MAX_FALL_SPEED
+
+	# used for animation facing
+	if facingRight == true:
+		sprite.scale.x = 1
+	else:
+		sprite.scale.x = -1
+
 	# horizontal movement
 	if Input.is_action_pressed("right"):
 		motion.x += ACCELERATION
@@ -86,20 +88,15 @@ func _physics_process(delta):
 		facingRight = false
 		if is_on_floor() && anim.current_animation != "Dash":
 			anim.play("Run")
-	elif !dashing: # slow down if not running
+	else: # slow down if not running
 		motion.x = lerp(motion.x, 0, 0.2)
 		if is_on_floor() && anim.current_animation != "Dash":
 			anim.play("Idle")
-	
+
 	# limit to max speed if not dashing
-	if hasLanded:
-		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
-		#print(motion.x)
-		pass
-	else:
-		motion.x = clamp(motion.x, -DASH_SPEED, DASH_SPEED)
-		motion.y = clamp(motion.y, -DASH_SPEED, DASH_SPEED)
-	
+	motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+	#print(motion.x)
+
 	# jumping
 	if is_on_floor(): # can only jump if on floor
 		falling = false
@@ -111,43 +108,6 @@ func _physics_process(delta):
 		elif (motion.y > 0 && !falling) && anim.current_animation != "Dash":
 			anim.play("Fall")
 			falling = true
-	
+
 	# apply movement
 	motion = move_and_slide(motion, UP)
-
-#func _input(event):
-#	# determine hasLanded
-#	if is_on_floor():
-#		hasLanded = true
-#
-#	# handle dash + associated animation
-#	if event is InputEventMouseButton and dashDelayTimer.is_stopped() and hasLanded: # on click
-#		# hasn't landed since dashing yet
-#		print("Dashing")
-#		hasLanded = false
-#
-#		# start timer
-#		dashDelayTimer.start(dashDelay)
-#
-#		# play sound effect
-#		$AudioStreamPlayer2D.play()
-#
-#		# get local mouse direction, calculate motion
-#		var mouse_direction = get_local_mouse_position().normalized()
-#		motion = Vector2(mouse_direction.x, mouse_direction.y) * MAX_SPEED/2
-#		print("Mouse position:\t" + String(get_viewport().get_mouse_position()))
-#		print("Global Transform:\t\t\t" + String(sprite.position))
-#
-#		# used for animation facing
-#		if mouse_direction.x > sprite.position.x: # dashing right
-#			facingRight = true
-#			sprite.scale.x = 1
-#		elif mouse_direction.x < sprite.position.y: # dashing left
-#			facingRight = false
-#			sprite.scale.x = -1
-#
-#		# animate
-#		anim.play("Dash")
-#
-#		# apply movement
-#		move_and_collide(motion)
