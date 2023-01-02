895c002a55206a02789701fe31e08301c67cc156
Sulfurous-Impersonation <june@watson-net.com>
2023-01-01 21:45:53 -0800

    updated tileset

--- CameraController.gd
+++ CameraController.gd
@@ -12,7 +12,7 @@ onready var player := $"../Player"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	limit_bottom = 32
+	limit_bottom = 64
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
