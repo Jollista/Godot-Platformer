cf5d52584525861d990b8fa919c4e7241bdf277c
Sulfurous-Impersonation <june@watson-net.com>
2023-01-14 23:56:50 -0800

    moved camera limits from script to scene dependent

--- Scripts/CameraController.gd
+++ Scripts/CameraController.gd
@@ -9,12 +9,6 @@ var playerPos
 
 onready var player := $"../Player"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	limit_bottom = 64
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# get mouse position and player position
